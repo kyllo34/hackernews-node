cd8fa535f0b64efcc2e6a9441801f3be681746e0
Andrew Kyllo <kyllo34@gmail.com>
2020-05-14 17:45:35 -0700

    added User type to datamodel

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 const { GraphQLServer } = require('graphql-yoga')
+const { prisma } = require('./generated/prisma-client')
 
-
-
-let idCount = links.length
 
 const resolvers = {
   Query: {
@@ -23,7 +21,8 @@ const resolvers = {
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
-  resolvers
+  resolvers,
+  context: { prisma },
 })
 
 server.start(() => console.log(`Server is running on http://localhost:4000`))
