f8095a5d6bb43d3029102f7cc00e3cbde1766abd
Andrew Kyllo <kyllo34@gmail.com>
2020-05-18 17:08:13 -0700

    added voting functionality

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -9,6 +9,19 @@ const newLink = {
   },
 }
 
+function newVoteSubscribe(parent, args, context, info) {
+  console.log(context.prisma.$subscribe)
+  return context.prisma.$subscribe.vote({ mutation_in: ['CREATED'] }).node()
+}
+
+const newVote = {
+  subscribe: newVoteSubscribe,
+  resolve: payload => {
+    return payload
+  },
+}
+
 module.exports = {
   newLink,
+  newVote,
 }
